fd1aa5c6597afd26c15447c9eae89e6fd114aeb5
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-10-18 15:13:14 -0400

    Defining constants for components

---
+++ src/assets/constants.ts
@@ -0,0 +1,7 @@
+export const taglines = [
+    "Coding for the love of the game",
+    "Building software, one bug at a time",
+    "Creating digital experiences that matter",
+    "From ideas to deployed apps",
+    "Developer by day, JRPG enthusiast by night",
+];
