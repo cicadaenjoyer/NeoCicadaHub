de199164b2c8780db59479e3c62c332445f9fbac
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-10-18 12:05:23 -0400

    Creating a new navigation bar component

---
+++ src/components/navigation/NavigationBar.tsx
@@ -0,0 +1,29 @@
+// Styling
+import "../../styles/navigation/nagivation.styles.css";
+
+// Components
+import TabButton from "./TabButton";
+
+// Hooks
+import useWindowDimensions from "../../hooks/WindowDimensions";
+
+/**
+ * Renders the main navigation bar with tab buttons for different sections.
+ * The navigation bar's width dynamically adjusts to the current window dimensions.
+ *
+ * @returns {JSX.Element} The navigation bar component.
+ */
+function NavigationBar() {
+    const { width } = useWindowDimensions();
+
+    return (
+        <div style={{ width: `${width}px` }} className="bar">
+            <TabButton label="Home" />
+            <TabButton label="Work" />
+            <TabButton label="Projects" />
+            <TabButton label="About" />
+        </div>
+    );
+}
+
+export default NavigationBar;
