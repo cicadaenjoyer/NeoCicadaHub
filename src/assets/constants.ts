008046126303f4a79f23b59765ebbae068d083b7
cicadaenjoyer <abrahamgonzalez3764@gmail.com>
2025-10-25 12:06:10 -0700

    Added new music player component

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -5,3 +5,10 @@ export const taglines = [
     "From ideas to deployed apps",
     "Developer by day, JRPG enthusiast by night",
 ];
+
+export const tracks = [
+    {
+        title: "The Place You Must Return",
+        src: "Mega Man Star Force OST - T36_ The Place You Must Return (Credits Theme).mp3",
+    },
+];
